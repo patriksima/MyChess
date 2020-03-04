cc3e7a19b67acf9144c6782c2e5d0cec26056861
Patrik Šíma <172640+patriksima@users.noreply.github.com>
2020-03-04 20:39:28 +0100

    Code cleanup

--- Assets/Scripts/FileItem.cs
+++ Assets/Scripts/FileItem.cs
@@ -10,14 +10,4 @@ public enum ItemType
 public class FileItem : MonoBehaviour
 {
     public ItemType ItemType { get; set; }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
 }
