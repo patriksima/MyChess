fc8f426649b8048e98f06c14932d4fea046d52a5
Patrik Šíma <172640+patriksima@users.noreply.github.com>
2020-03-05 19:33:18 +0100

    Singleton

---
+++ Assets/Scripts/SIngleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static readonly Lazy<T> LazyInstance = new Lazy<T>(CreateSingleton);
+
+    public static T Instance => LazyInstance.Value;
+
+    private static T CreateSingleton()
+    {
+        var instance = FindObjectOfType<T>(); // because it already exists in scene
+        DontDestroyOnLoad(instance.gameObject);
+        return instance;
+    }
+}
