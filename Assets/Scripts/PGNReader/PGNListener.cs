5e642fce8e97b3642a3057ea8896150c61924947
Patrik Šíma <172640+patriksima@users.noreply.github.com>
2020-03-07 17:29:57 +0100

    Remove CLSCompliant

--- Assets/Scripts/PGNReader/PGNListener.cs
+++ Assets/Scripts/PGNReader/PGNListener.cs
@@ -28,7 +28,6 @@ using IToken = Antlr4.Runtime.IToken;
 /// <see cref="PGNParser"/>.
 /// </summary>
 [System.CodeDom.Compiler.GeneratedCode("ANTLR", "4.8")]
-[System.CLSCompliant(false)]
 public interface IPGNListener : IParseTreeListener {
 	/// <summary>
 	/// Enter a parse tree produced by <see cref="PGNParser.parse"/>.
